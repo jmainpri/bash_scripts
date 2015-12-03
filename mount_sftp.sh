e0420543a258ec1e8839b26b2c5d8036d7582ad2
Jim Mainprice <jmainprice@wpi.edu>
2015-12-03 09:54:17 +0100

    fix sshfs script

--- mount_sftp.sh
+++ mount_sftp.sh
@@ -1,1 +1,1 @@
-sshfs -o reconnect -o volname=Mondrian -o IdentityFile=~/.ssh/id_rsa mondrian.is.localnet /Users/jmainpri/Mondrian
+sshfs -o reconnect -o volname=Mondrian -o IdentityFile=~/.ssh/id_rsa mondrian.is.localnet: /Users/jmainpri/Mondrian
