8c60f0bb392ac8b02fe2d4162ab50e926c34d1e4
Jim Mainprice <jmainprice@wpi.edu>
2016-01-31 20:34:51 +0100

    Add command for executing ioc validation

--- mount_sftp.sh
+++ mount_sftp.sh
@@ -1,1 +1,4 @@
-sshfs -o reconnect -o volname=Mondrian -o IdentityFile=~/.ssh/id_rsa mondrian.is.localnet: /Users/jmainpri/Mondrian
+#!/bin/bash
+# $1 : machine_ip
+# $2 : volume_name
+sshfs -o reconnect -o volname=$2 -o IdentityFile=~/.ssh/id_rsa $2: $HOME/$1
---
+++ remote_execute_command.sh
@@ -0,0 +1,3 @@
+#bin/sh
+FOLDER=/usr/local/jim_local/Dropbox/move3d/move3d-launch/move3d-python/ioc/
+ssh -n -f mondrian.is.localnet "sh -c 'cd ${FOLDER}; nohup ./ioc_leave_one_out_trajectories.py > /dev/null 2>&1 &'"
