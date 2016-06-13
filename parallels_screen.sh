a185f835f03a954c7885a3fb30ef2c7805d77291
Jim Mainprice <mainprice@gmail.com>
2016-06-13 22:31:20 +0200

    Paralles

--- parallels_screen.sh
+++ parallels_screen.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # First add these lines to add the configuration
 # to the default screen, default can be replaced by VGA1 in some cases
-xrandr --newmode "2560x1440_40.00"  201.00  2560 2720 2984 3408  1440 1443 1448 1476 +hsync +vsync
-xrandr --addmode default 2560x1440_40.00
+
+#xrandr --newmode "2560x1440_40.00"  201.00  2560 2720 2984 3408  1440 1443 1448 1476 +hsync +vsync
+#xrandr --addmode default 2560x1440_40.00
+
+
+xrandr --newmode "1920x1080_40.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --addmode default 1920x1080_40.00
 
 # once this is done you can, go in the Display option of Ubuntu
 # The new screen option should appear, the:
