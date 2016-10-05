3c0d6f3d9576a5c94e0d34086b64cc0d463e52a8
Jim Mainprice <mainprice@gmail.com>
2016-10-05 16:11:59 +0200

    make script exec

--- video_downgrade.sh
+++ video_downgrade.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # avconv -i $1 -c:v libx264 -s 416x234 -b 10000k $1_downgrade.mp4
-avconv -i $1 -c:v libx264 -s 640x360 -b 6000k $1_downgrade.mp4
+# avconv -i $1 -c:v libx264 -s 640x360 -b 6000k $1_downgrade.mp4
+avconv -i $1 -c:v libx264 -s 1248x776 -b 12000k $1_downgrade.mp4
