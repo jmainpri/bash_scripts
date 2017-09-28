32eddd1b30ded56a85706c28aa1525b495ca3a04
Jim Mainprice <mainprice@gmail.com>
2017-09-28 15:37:27 +0200

    Add build classic script

--- parallels_screen.sh
+++ parallels_screen.sh
@@ -2,9 +2,13 @@
 # First add these lines to add the configuration
 # to the default screen, default can be replaced by VGA1 in some cases
 
-xrandr --newmode "2560x1440_40.00"  201.00  2560 2720 2984 3408  1440 1443 1448 1476 +hsync +vsync
-xrandr --addmode Virtual1 2560x1440_40.00
-xrandr --output Virtual1 --mode 2560x1440_40.00
+# xrandr --newmode "2560x1440_40.00"  201.00  2560 2720 2984 3408  1440 1443 1448 1476 +hsync +vsync
+# xrandr --addmode Virtual1 2560x1440_40.00
+# xrandr --output Virtual1 --mode 2560x1440_40.00
+
+xrandr --newmode "2880x1800_40.00"  201.00 2880 2720 2984 3408  1800 1443 1448 1476 +hsync +vsync
+xrandr --addmode Virtual1 2880x1800_40.00
+xrandr --output Virtual1 --mode 2880x1800_40.00
 
 #xrandr --newmode "1920x1080_40.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 #xrandr --addmode default 1920x1080_40.00
