a41f46da20c44b6f7a25790db55536aa7444d1ea
Jim Mainprice <mainprice@gmail.com>
2019-11-04 10:49:07 +0100

    Make a scaling script for ubuntu

--- scale_gnome_ui.sh
+++ scale_gnome_ui.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
+
+display_usage() { 
+    echo "Scaling of the Gnome GUI for using on 4k"
+    echo "pass argument *normal* to get original scaling"
+    echo -e "\nUsage:\n${BASH_SOURCE[0]} [normal/zoom] \n" 
+    }
+
+# if less than two arguments supplied, display usage 
+if [  $# -le 0 ] ; then 
+    display_usage
+    exit 1
+fi
+
+# check whether user had supplied -h or --help . If yes display usage 
+if [[ ( $# == "--help") ||  $# == "-h" ]] ; then 
+    display_usage
+    exit 0
+fi
+
+case "$1" in
+
+    "normal")
+	scaling=1. ;;
+
+    "zoom")
+	scaling=1.6 ;;
+    *)
+	display_usage
+	exit 1 ;;
+esac
+
+gsettings set org.gnome.desktop.interface text-scaling-factor $scaling
+# sudo emacs -nw /usr/share/applications/spotify.desktop
